92e9cbdef3eb8ba98f55488ef2344011583b578d
Alishan Shaikh <rwbn1.alishan.as@gmail.com>
2025-07-11 15:52:57 +0530

    data types

--- PYTHON/01_py_intro/index.py
+++ PYTHON/01_py_intro/index.py
@@ -3,3 +3,11 @@ print("hi everyone")
 print("how you doing..!")
 
 print("kem chho ", 100-20)
+
+print("red", "white")
+
+print("red", "white", sep=" & ")
+
+print("Graphic", end=" | "); print("UI/UX", end=" | "); print("Full STack", end="\n");
+
+print("Graphic", "UI/UX", "FUll STack", "FrontEnd", sep=", ");
---
+++ PYTHON/02_py_types/index.py
@@ -0,0 +1,39 @@
+
+
+# int
+# str// double quote '', "", ''' ''', """ """
+# float
+# complex
+
+
+a = input("A= ")
+b = input("B= ")
+
+
+a = int(a)
+b = int(b)
+
+
+print("A + B = ", a+b)
+print("A - B = ", a-b)
+print("A * B = ", a*b)
+print("A / B = ", a/b)
+print("A / B = ", a**b)  # exponential
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
