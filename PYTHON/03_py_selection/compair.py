f86b3e5036911e6eecaa4027f8c711c9a4b1f61b
Alishan Shaikh <rwbn1.alishan.as@gmail.com>
2025-07-14 15:59:13 +0530

    selection

---
+++ PYTHON/03_py_selection/compair.py
@@ -0,0 +1,14 @@
+
+num = 10;
+
+if(num == 1):
+    print("1 is pressed")
+
+elif(num == 2):
+    print("2 is pressed")
+
+elif(num == 3):
+    print("3 is pressed")
+
+else:
+    print("invalid choice")
---
+++ PYTHON/03_py_selection/index.py
@@ -0,0 +1,14 @@
+
+a = input("Enter A: ")
+b = input("Enter B: ")
+
+
+
+if(a > b):
+    print("A is Greater than B")
+
+elif(b > a):
+    print("B is Greater than A")
+
+else:
+    print("A & B are Equal...!")
